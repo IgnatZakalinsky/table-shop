d11fc3a90615f916c28bb75ed684aaa70aaaab09
Ignat <ai73a@yandex.ru>
2019-12-27 15:21:53 +0300

    Updates

---
+++ precache-manifest.4f0cabc9ae7d63f43c1a3b8cade7d5b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fa5212eacbb6e5aba67d0d774a775f",
+    "url": "/table-shop/index.html"
+  },
+  {
+    "revision": "d7fea2dd8d7809347d8b",
+    "url": "/table-shop/static/css/2.826c19ff.chunk.css"
+  },
+  {
+    "revision": "7a1604ede89c8fcff8eb",
+    "url": "/table-shop/static/css/main.ad277072.chunk.css"
+  },
+  {
+    "revision": "d7fea2dd8d7809347d8b",
+    "url": "/table-shop/static/js/2.afc968d3.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/table-shop/static/js/2.afc968d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a1604ede89c8fcff8eb",
+    "url": "/table-shop/static/js/main.f8481124.chunk.js"
+  },
+  {
+    "revision": "bd0ba916b69d0e238c63",
+    "url": "/table-shop/static/js/runtime-main.c83916fb.js"
+  }
+]);
